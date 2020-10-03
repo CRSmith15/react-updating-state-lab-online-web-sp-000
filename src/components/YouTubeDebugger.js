64bb8c292911b228c0b8437ebcde563a8eef4384
Chance Smith <chancesmith97@hotmail.com>
2020-10-03 00:41:37 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,38 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+export default class YouTubeDebugger extends Component {
+  constructor() {
+    super();
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video : {
+          resolution: '1080p'
+        }
+      }
+    };
+  }
+
+  handleBitrate = () => {
+    this.setState({
+      settings: {...this.state.setting, bitrate: 12}
+    })
+  }
+
+  handleResolution = () => {
+    this.setState({
+      setting: {...this.state.setting, video: {resolution: '720p'}}
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.handleBitrate}>{this.state.settings.bitrate}</button>
+        <button className="resolution" onClick={this.handleResolution}>{this.state.settings.resolution}</button>
+    )
+  }
+}
